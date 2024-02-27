a384c7f0be89cf2f4f844c785dee01984bfaddad
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-02-26 23:40:16 -0300

    update: correção variavel signUp

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -45,9 +45,9 @@ class FirebaseAuthService implements AuthService {
         await result.user!.updateDisplayName(name);
         log("Usuário $name Registrado com sucesso");
         return UserModel(
-            name: result.user!.displayName,
-            email: result.user!.email,
-            id: result.user!.uid);
+            name: _auth.currentUser?.displayName,
+            email: _auth.currentUser?.email,
+            id: _auth.currentUser?.uid);
       } else {
         throw Exception();
       }
